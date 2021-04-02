aee58b0952c55ad23cc6f9cbf9e233f12753e935
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-02 19:12:20 +0700

    chore(functions/cron): define `n`

--- functions/cron/index.js
+++ functions/cron/index.js
@@ -7,12 +7,11 @@ exports.scheduler = functions.pubsub
   .schedule("every 5 minutes") // At midnight.
   .timeZone("Asia/Bangkok")
   .onRun(async (_) => {
-    try {
-      const topic = pubsub.topic("fibonacci");
-      const message = {
-        data: Math.floor(Math.random() * 31),
-      };
-      await topic.publish(Buffer.from(JSON.stringify(message), "utf-8"));
-    } catch (_) {}
-    return null;
+    const n = Math.floor(Math.random() * 31);
+    console.log(n);
+    const topic = pubsub.topic("fibonacci");
+    const message = {
+      data: n,
+    };
+    await topic.publish(Buffer.from(JSON.stringify(message), "utf-8"));
   });
